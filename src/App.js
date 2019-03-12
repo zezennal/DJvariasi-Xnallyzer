744d37a85735dd84338d6ac2be0963aaeafc3127
nall <zezennal@gmail.com>
2019-03-12 11:34:56 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Users from './components/users/Users';
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Users title="Users List"/>
+        <Header/>
       </div>
     );
   }
